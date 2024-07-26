d40a9963372b83881d382debbc33b9ab8d397bed
Kleber Coelho <klebercoelhogt@gmail.com>
2024-07-25 23:39:14 -0300

    #

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,10 @@ const Home = () => {
     <div>
       <h1>teste</h1>
       <a href="/login">LOGIN</a>
+      <div>
+        <a href="/register">register</a>
+      </div>
+      <a href="/dashboard">dashboard</a>
     </div>
   );
 };
